1804969eea96bb1edb59c5947677757b1e97ab2e
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-08 01:30:07 +0400

    feat: added 8.js file and did tasks

---
+++ 8.js
@@ -0,0 +1,14 @@
+//"use strict"
+
+// function isIsogram(str) {
+
+//     let word = str.split("");
+//     for (let i = 0, j = 1; i < word.length - 1; i++, j++) {
+//       if (word[i] == word[j]){
+//         return "not isogram"    
+//       }
+//     }
+//     return "isogram"   
+//    }
+   
+//    console.log(isIsogram(""))
