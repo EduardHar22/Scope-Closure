c16f0ea782935a9837af73f0cbc235babd62c9fc
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-08 01:29:07 +0400

    feat: added 5.js file and did tasks

---
+++ 5.js
@@ -0,0 +1,9 @@
+//"use strict"
+
+// function filterFalsyValues(arr){
+//     let filtered = arr.filter(Boolean);
+//     return filtered;
+    
+// }
+// const values = [null, true, {}, { name: "Elon" }, "", NaN, 0];
+// console.log(filterFalsyValues(values));
