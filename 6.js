a26b025084caef66cd58d2dc3461984038dfaf51
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-08 01:29:18 +0400

    feat: added 6.js file and did tasks

---
+++ 6.js
@@ -0,0 +1,7 @@
+// "use strict"
+
+// function lengthOf(arr){
+//     let finished = arr.filter(word => word.length > 3);
+//     return finished;
+// }
+// console.log(lengthOf(["kia", "tesla", "bmw", "mercedes"]))
