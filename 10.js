be8fca8a7f73e908b02a4f3ceb052dfc34e500d5
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-08 01:30:33 +0400

    feat: added 10.js file and did tasks

---
+++ 10.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// function uniqueOrNot(arr) {
+//   let result = "";
+//   arr.sort((a, b) =>  a - b);
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === arr[i + 1]) return 'nonUniqu';
+//     else result = 'unique';
+//   }
+//   return  result;
+// }
+// console.log(uniqueOrNot([1, 4, -2, 11, 5]));
+
+
+// function isPalindrome(word){
+//   let lowCaseWord = word.toLowerCase();
+//   let reversedWord = lowCaseWord.split("").reverse().join('');
+
+//   if(lowCaseWord === reversedWord)    return 'yes';
+//   return 'no';
+// }
+// console.log(isPalindrome("assdddssA")); 
