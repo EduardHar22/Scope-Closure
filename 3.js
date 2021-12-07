7e02676775c3876fd1f4e999fc651f202dd94a01
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-08 01:28:40 +0400

    feat: added 3.js file and did tasks

---
+++ 3.js
@@ -0,0 +1,12 @@
+//"use strict"
+
+// function add_suffix(suf){
+//     return function(word){
+//         return word + suf;
+//     }
+// }
+// const add_ly = add_suffix("ly");
+// console.log(add_ly("hopeless"));
+
+// const add_less = add_suffix("less");
+// console.log(add_less("fear"))
